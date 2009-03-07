5c923cc8b1c41b3f45e44dabfd62c89cd5db6ae0
martind <martin@dekstop.de>
2009-03-07 22:27:52 +0000

    don't log in updateloop script
    git-svn-id: file:///Users/mongo/data/backups/websites/chilco-textdrive/svn-repo-dump-restored/code/py-feedcache/trunk@96 95e7b18c-8410-0410-b04a-c7d263c0d358

--- bin/updateloop.sh
+++ bin/updateloop.sh
@@ -3,10 +3,7 @@
 bin=`dirname $0`
 bin=`cd $bin; pwd`
 
-LOGDIR=`cd $bin; cd ../log/; pwd`
-
 (while [ true ]; do
 	nice -5 ${bin}/feedcache.sh update || exit 1
 	sleep 1800
-done) 2>&1 | tee -a ${LOGDIR}/feedcache.log
-
+done)
