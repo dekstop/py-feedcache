da00182097190901367fa5c609cfef421994b62d
martind <martin@dekstop.de>
2009-07-13 23:32:10 +0000

    shorter update delays
    git-svn-id: file:///Users/mongo/data/backups/websites/chilco-textdrive/svn-repo-dump-restored/code/py-feedcache/trunk@165 95e7b18c-8410-0410-b04a-c7d263c0d358

--- bin/updateloop.sh
+++ bin/updateloop.sh
@@ -7,5 +7,5 @@ bin=`cd $bin; pwd`
 	nice -5 ${bin}/feedcache.sh update || exit 1
 	nice -5 ${bin}/feedcache.sh index || exit 1
 	nice -5 ${bin}/feedcache.sh uniq musicblogs --log-sql || exit 1
-	sleep 1800
+	sleep 800
 done)
